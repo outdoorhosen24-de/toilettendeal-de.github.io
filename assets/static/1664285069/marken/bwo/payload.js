653bab79a9a3473e2694cdcf8197b7f72e74baab
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 13:27:51 +0000

    deploy: 3066153b12d56a2f6beceb370e65406972c09545

---
+++ assets/static/1664285069/marken/bwo/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken/bwo", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/wc-sitz-parima-mit-absenkautomatik-polypropylen-weisz-bwo-3990389-1.jpg":"\u002Fassets\u002Fimages\u002Fwc-sitz-parima-mit-absenkautomatik-polypropylen-weisz-bwo-3990389-1.webp"}}],fetch:{},mutations:void 0});
